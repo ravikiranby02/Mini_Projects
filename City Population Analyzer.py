ea2d2d3771c38514ce663c386429ccb3709b2dff
Ravi Kiran B Y <ravikiranby02@gmail.com>
2026-01-18 19:46:08 +0530

    Update City Population Analyzer.py

--- City Population Analyzer.py
+++ City Population Analyzer.py
@@ -1,32 +1,32 @@
 """City Population Analyzer"""
-# cities = {
-#     "Bengaluru":8443675,
-#     "Mysuru":920550,
-#     "Chennai":7090000,
-#     "Delhi":16787941,
-#     "Mumbai":12442373
-# }
-# filter_cities= {city:pop for city, pop in cities.items() if pop > 8000000}
-# print(filter_cities)
+cities = {
+    "Bengaluru":8443675,
+    "Mysuru":920550,
+    "Chennai":7090000,
+    "Delhi":16787941,
+    "Mumbai":12442373
+}
+filter_cities= {city:pop for city, pop in cities.items() if pop > 8000000}
+print(filter_cities)
 
 
-# def add_city(cities,name,population):
-#     cities[name] = population
-#     print(f"{name} added successfully.")
+def add_city(cities,name,population):
+    cities[name] = population
+    print(f"{name} added successfully.")
 
-# def delete_city(cities,name):
-#     if name in cities:
-#         del cities[name]
-#         print(f"{name} deleted successfully.")
-#     else:
-#         print("City not found.")
+def delete_city(cities,name):
+    if name in cities:
+        del cities[name]
+        print(f"{name} deleted successfully.")
+    else:
+        print("City not found.")
 
-# def search_city(cities,name):
-#     if name in cities:
-#         print(f"{name} population is {cities[name]}")
-#     else:
-#         print("city not found.")
+def search_city(cities,name):
+    if name in cities:
+        print(f"{name} population is {cities[name]}")
+    else:
+        print("city not found.")
 
-# add_city(cities,"K R Nagara",500000)
-# delete_city(cities,"Mysuru")
-# search_city(cities,"Bengaluru")
+add_city(cities,"K R Nagara",500000)
+delete_city(cities,"Mysuru")
+search_city(cities,"Bengaluru")
